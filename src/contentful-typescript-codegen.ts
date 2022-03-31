72528e3af9dac4f7ab7e00f8cf18ce481f535dd2
Kyohei Hamaguchi (@tnzk) <tnzk@users.noreply.github.com>
2022-03-31 11:09:42 +0900

    Use .cjs filename

--- src/contentful-typescript-codegen.ts
+++ src/contentful-typescript-codegen.ts
@@ -60,7 +60,7 @@ const cli = meow(
 )
 
 async function runCodegen(outputFile: string) {
-  const getEnvironmentPath = path.resolve(process.cwd(), "./getContentfulEnvironment.js")
+  const getEnvironmentPath = path.resolve(process.cwd(), "./getContentfulEnvironment.cjs")
   const getEnvironment = require(getEnvironmentPath)
   const environment = await getEnvironment()
   const contentTypes = await environment.getContentTypes({ limit: 1000 })
